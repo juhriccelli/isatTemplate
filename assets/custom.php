ebd134e24f8db3e38dc4856d3b5041b769c0cff1
Juliano Ricceli da Silva <julianoricceli@Mac-mini-de-Juliano.local>
2020-02-09 13:17:10 -0300

    Versão 1.2

--- assets/custom.php
+++ assets/custom.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="assets/css/estilo.css">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato" />
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <script src="assets/js/jquery.js"></script>
   <script src="assets/js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -9,19 +9,32 @@
 <body>
   <div class="base">
     <div class="centro">
-      <div class="colunas">
-tela 1
-      </div>
-      <div class="seta-baixo">
+      <div class="secao visible">
+        <div class="conteudo">
+          <div class="p40">
+            <p class="titulo">
+              Você conhece a ISAT?
+            </p>
 
-      </div>
-    </div>
-    <div class="centro">
-      <div class="colunas">
-tela2
-      </div>
-      <div class="seta-baixo">
+            <p class="texto">
+              Com uma base de mais 100 clientes atendidos no Brasil nas áreas de treinamento, recursos humanos, trade marketing, compliance e auditoria e marketing, entendemos que o conteúdo e a tecnologia caminham juntos, cada vez mais fortalecendo nossa posição no mercado.
+            </p>
+          </div>
+          <div class="p60">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/hwtitTdVR2s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+          </div>
+        </div>
+        <div class="seta-baixo">
 
+        </div>
+
+        <div class="secao">
+          <div class="conteudo">
+            teste2
+          </div>
+          <div class="seta-baixo">
+
+          </div>
       </div>
     </div>
   </div>
